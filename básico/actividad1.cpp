d3231581da85e74cf1f7b29c1f1042ff87a42b4d
Jeyssson <61421639+jeyssson4k@users.noreply.github.com>
2021-12-26 18:13:04 -0500

    upload tablasDeMultiplicar

---
+++ básico/actividad1.cpp
@@ -0,0 +1,40 @@
+//TABLAS DE MULTIPLICAR
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int a=1;
+	cout<<"Tablas de multiplicar"<<endl;
+	
+			
+	return 0;
+	
+	//JEYSSON FERNANDEZ LLANES
+}
+int tablas(){
+			for(i=0; i<10; i++){
+				b=a*i;
+				cout<<a<<"X"<<i"="<<b<<endl;
+			}
+		}
+	while(a!=0){
+		cout<<"tabla del 1"<<endl;
+		cout<<"tabla del 2"<<endl;
+		cout<<"tabla del 3"<<endl;
+		cout<<"tabla del 4"<<endl;
+		cout<<"tabla del 5"<<endl;
+		cout<<"tabla del 6"<<endl;
+		cout<<"tabla del 7"<<endl;
+		cout<<"tabla del 8"<<endl;
+		cout<<"tabla del 9"<<endl;
+		
+		cout<<"digite el número que desea:"<<endl;
+		cin>>a;
+		
+			
+		
+	}
+}
+tablas();
